d2ad90ed6bbf1b2be84e98f755cc8b8d2cf34495
stevejr <>
2018-09-04 19:56:29 +0100

    Updated output message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World from Jenkins World 2018!";
+    private final String message = "Hello World from Jenkins World Nice 2018!";
 
     public App() {}
 
